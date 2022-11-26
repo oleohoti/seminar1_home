7feb5a7c5267ea479ea73621839cace2f1bfcc77
Vladimir <vova.okhotin@mail.ru>
2022-11-26 22:55:44 +0800

    Add ex1

---
+++ exemple001/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num*num;
+Console.WriteLine("Квадрат введенного числа равен "+square); 
+Console.WriteLine($"Квадрат введенного числа равен {square} "); 
